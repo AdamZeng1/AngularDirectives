52fa097262f35ad13e4a7851f38683f6b28974a3
adamzeng <khklara3@gmail.com>
2019-11-12 11:12:06 +0800

    using-service-rather-than-output-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless.directive';
+import {InactiveUsersComponent} from './inactive-users/inactive-users.component';
+import {ActiveUsersComponent} from './active-users/active-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
-    UnlessDirective
+    UnlessDirective,
+    ActiveUsersComponent,
+    InactiveUsersComponent
   ],
   imports: [
     BrowserModule
